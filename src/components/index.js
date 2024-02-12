90a2b52c60a90ffd6165218b672c72b00c07922b
Abdullah Azhar Khan <abdullahazharkhan1@gmail.com>
2024-02-12 23:33:01 +0500

    added pages

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,28 @@ import Container from "./Container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
+import PostForm from "./PostForm/PostForm";
+import AuthLayout from "./AuthLayout";
+import Button from "./Button";
+import Input from "./Input";
+import Login from "./Login";
+import PostCard from "./PostCard";
+import Select from "./Select";
+import Signup from "./Signup";
 
-export { Header, Footer, Container, Logo, LogoutBtn, RTE };
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  RTE,
+  PostForm,
+  AuthLayout,
+  Button,
+  Input,
+  Login,
+  PostCard,
+  Select,
+  Signup,
+};
